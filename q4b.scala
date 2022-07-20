fe6959ae2e7403d544de60fa06c83add6463a578
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2022-07-20 17:10:01 +0530

    Update q4b.scala

--- q4b.scala
+++ q4b.scala
@@ -1,24 +1,10 @@
-import scala.io.StdIn.readInt;
+object q4b extends App{
 
-object q4b extends App {
-  def profit(ticketPrice: Int, attendance: Int) : Int = ticketPrice*attendance;
-
-
-  def attendanceChange(ticketPrice: Int) : Int = {
-    if(ticketPrice > 15)
-      return ((ticketPrice - 15) / 5) * (-20);
-    else if(15 > ticketPrice)
-      return ((15 - ticketPrice) / 5) * 20;
-    else
-      return 0;
-  }
-
-  def netProfit(ticketPrice: Int, attendance: Int) : Int = {
-    return profit(ticketPrice, attendance + attendanceChange(ticketPrice)) - (500 + 3*(attendance + attendanceChange(ticketPrice)));
-  }
-
-  print("Enter price of a ticket:");
-  var ticketPrice = readInt();
-  print("Profit : " + netProfit(ticketPrice, 120));
-
+  def attendance(price:Float):Float=120+(15-price)/5*20
+  def earn(price:Float):Float=price*attendance(price)
+  def cost(price:Float):Float=500+attendance(price)*3
+  def profit(price:Float):Float=earn(price)-cost(price)
+  
+  println(profit(24))
+  
 }
